d209b122931c75510bc871badc27d062eba26401
Andriy <shmagala2001@gmail.com>
2022-04-16 23:29:39 +0300

    add php mailer

--- scripts/mailer.php
+++ scripts/mailer.php
@@ -1,8 +1,43 @@
 <?php
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
 $to      = 'shmagala@gmail.com';
 $subject = $_POST['title'];
 $message = 'hello';
 $headers = $_POST['resultText']
 
-mail($to, $subject, $message, $headers);
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'shmagala2001@gmail.com'; // Логин на почте
+    $mail->Password   = 'a250213shua'; // Пароль на почте
+    $mail->SMTPSecure = 'tls';
+    $mail->Port       = 587;
+    $mail->setFrom('mail@gmail.com', 'Имя отправителя'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('shmagala2001@gmail.com');
+
+    // Прикрипление файлов к письму
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+    
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+}
+
+
 ?>
